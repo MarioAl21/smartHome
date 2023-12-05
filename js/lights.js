cdd5a95c03a5000ee3d3c9bbe7d0ac60e91a4d08
Mario A. Núñez Zavala <mario.nunez@alumno.buap.mx>
2023-12-04 19:47:27 -0600

    Add perseverance of the lights logs and remove button to erase lights

--- js/lights.js
+++ js/lights.js
@@ -21,11 +21,14 @@ class LightsHub {
     controlLights(light_name, state) {
         const light = this.lights[light_name];
 
-        if (light) {
-            light.setState(state);
-            this.notifyObservers();
-        } else 
-            console.log(`Light ${lightName} not found.`);
+        console.log(this.lights[light_name]);
+
+        if (light instanceof Light) {
+          light.setState(state);
+          smartHub.notifyObservers(); // Notify observers after controlling the light
+        } else {
+          console.log(`Light ${light_name} not found or is not an instance of Light.`);
+        }
     }
 
     // Adds an observer to the list of obsevers
@@ -42,6 +45,13 @@ class LightsHub {
     notifyObservers() {
         this.observers.forEach(observer => observer.update(this.lights));
     }
+
+    removeLight(lightName) {
+      delete this.lights[lightName];
+      this.notifyObservers();
+    }
+
+    display() { console.log(this.lights); }
 };
 
 // Observer pattern for UI updates
@@ -67,6 +77,7 @@ class UIObserver {
           <h2>${light.name}</h2>
           <p>State: ${light.state}</p>
           <button class="turnLights">Turn On/Off</button>
+          <button class="removeLight">Remove Light</button>
         `;
         this.app.appendChild(lightDiv);
       });
@@ -89,6 +100,19 @@ class Light {
      this.state = newState;
      console.log(`${this.name} is now ${this.state}.`);
     }
+
+     // Custom serialization method
+    toJSON() {
+     return { name: this.name, state: this.state };
+    }
+
+    // Custom deserialization method
+    static fromJSON(data) {
+     const light = new Light(data.name);
+     light.setState(data.state);
+     console.log(light);
+     return light;
+    }
 }
 
 
@@ -108,28 +132,10 @@ const kitchenLight = new Light('Kitchen Light');
 smartHub.addLight('Living Room Light', livingRoomLight);
 smartHub.addLight('Kitchen Light', kitchenLight);
 
-// Trigger a control action
-const lightsContainer = document.getElementById('lightsContainer');
 
-let state = false;
-lightsContainer.addEventListener('click', (event) => {
-    if (event.target.classList.contains('turnLights')) {
-        //console.log(event.target);
-        const lightName = event.target.parentNode.querySelector('h2').textContent;
-        state = state ^ true;
-        let action = 'ON';
-        if(!state)
-         action = 'OFF';
-        smartHub.controlLights(lightName, action);
-    }
-});
-
-// Add an event listener for the "Add Light" button
-const addLightButton = document.getElementById('addLightButton');
-addLightButton.addEventListener('click', addNewLight);
 
  // Function to add a new light
- function addNewLight() {
+function addNewLight() {
     const lightNameInput = document.getElementById('lightName');
     const initialStateSelect = document.getElementById('initialState');
 
@@ -143,6 +149,9 @@ addLightButton.addEventListener('click', addNewLight);
       // Add the new light to the hub
       smartHub.addLight(newLightName, newLight);
 
+      // Save lights to localStorage
+      saveLightsToStorage();
+
       // Clear the form fields
       lightNameInput.value = '';
       initialStateSelect.value = 'OFF';
@@ -153,6 +162,79 @@ addLightButton.addEventListener('click', addNewLight);
       alert('Please enter a valid light name and initial state.');
     }
   }
+
+// Function to save lights to localStorage
+function saveLightsToStorage() {
+  const serializedLights = JSON.stringify(Object.values(smartHub.lights).map(light => light.toJSON()));
+  localStorage.setItem('lights', serializedLights);
+}
+
+// Function to retrieve lights from localStorage
+function getLightsFromStorage() {
+  const lightsData = localStorage.getItem('lights');
+  if (lightsData) {
+    try {
+      const lightsArray = JSON.parse(lightsData);
+      if (Array.isArray(lightsArray)) {
+        return lightsArray.map(lightData => Light.fromJSON(lightData));
+      } else {
+        console.error('Invalid data structure in localStorage for lights.');
+        return [];
+      }
+    } catch (error) {
+      console.error('Error parsing lights data from localStorage:', error);
+      return [];
+    }
+  } else {
+    return [];
+  }
+}
+
+
+
+  // Load lights from localStorage when the page loads
+  smartHub.lights = getLightsFromStorage();
+  uiObserver.manualRender();
+
+  // Add an event listener for the "Add Light" button
+  const addLightButton = document.getElementById('addLightButton');
+  addLightButton.addEventListener('click', addNewLight);
+
+  // Remove light
+  const removeLightButtons = document.querySelectorAll('.removeLight');
+  removeLightButtons.forEach(removeLightButton => {
+    removeLightButton.addEventListener('click', function () {
+      // Get the light name from the parent element
+      const lightName = this.parentNode.querySelector('h2').textContent;
+
+      // Remove the light from the hub
+      smartHub.removeLight(lightName);
+
+      // Save updated lights to localStorage
+      saveLightsToStorage();
+
+      // Update the UI
+      uiObserver.manualRender();
+    });
+  });
+
+  // Trigger a control action
+const lightsContainer = document.getElementById('lightsContainer');
+
+let state = false;
+lightsContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('turnLights')) {
+        //console.log(event.target);
+        const lightName = event.target.parentNode.querySelector('h2').textContent;
+        // Remove the light from the hub
+        smartHub.removeLight(lightName);
+        state = state ^ true;
+        let action = 'ON';
+        if(!state)
+         action = 'OFF';
+        smartHub.controlLights(lightName, action);
+    }
+});
 
 // Manually render the UI
 uiObserver.manualRender();
